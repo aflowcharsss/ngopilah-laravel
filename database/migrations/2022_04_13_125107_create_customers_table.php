0def8c740c981abbefc70a3b11ef8dca091cbbc9
Afochar Syabnirusmanto <aflowcharsss@gmail.com>
2022-04-13 19:57:47 +0700

    Migrations : Customers

---
+++ database/migrations/2022_04_13_125107_create_customers_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            /**
+             * nama
+             * contact
+             * email
+             * alamat
+             * diskon
+             * tipe_diskon
+             * ktp
+             */
+            $table->id();
+            $table->string('name');
+            $table->integer('contact');
+            $table->string('email');
+            $table->string('address');
+            $table->integer('discount');
+            $table->enum('discount_type', ['percentage', 'fixed']);
+            $table->string('identity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
