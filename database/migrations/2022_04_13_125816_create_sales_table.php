5849050918b7f5c673bb7864ba987d95c2972c5a
Afochar Syabnirusmanto <aflowcharsss@gmail.com>
2022-04-13 20:13:39 +0700

    Migrations : Sales

---
+++ database/migrations/2022_04_13_125816_create_sales_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            /**
+             * code_transaksi
+             * tanggal_transaksi
+             * customer
+             * item 
+             * qty
+             * total_diskon
+             * total_harga
+             * total_bayar
+             */
+            $table->id();
+            $table->string('transaction_code');
+            $table->date('transaction_date');
+            $table->integer('quantity');
+            $table->integer('total_discount');
+            $table->integer('total_priice');
+            $table->integer('total_checkout');
+            $table->foreignId('item_id')->constrained('items')->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
